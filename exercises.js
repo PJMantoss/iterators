0555b804f4b2c73a0ebc7afa5a9a54a303f0a95e
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2020-02-24 19:30:30 +0100

    update exercises.js

--- exercises.js
+++ exercises.js
@@ -2,3 +2,11 @@
 Ex 1. Write a function called printFirstAndLast which accepts an array (of objects)
 and console.logs a new string with the first character and the last character of each value.
 */
+function printFirstAndLast(arr){
+    for (let i=0; i<arr.length; i += 1){
+        console.log(arr[i].charAt(0).concat(arr[i].charAt(arr[i].length - 1)));
+    }
+}
+
+//test
+printFirstAndLast(['awesome','example','of','forEach']); // ae, ee, of, fh
