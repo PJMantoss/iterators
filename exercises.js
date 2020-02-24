2810e907bfe0da78f167ec1ea3638347e7582d14
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2020-02-24 19:29:11 +0100

    create exercises.js

---
+++ exercises.js
@@ -0,0 +1,4 @@
+/*
+Ex 1. Write a function called printFirstAndLast which accepts an array (of objects)
+and console.logs a new string with the first character and the last character of each value.
+*/
